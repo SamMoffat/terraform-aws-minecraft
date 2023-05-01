d5d71ca654606dd058b85254aba3c111dc93536f
Misha Mamonov <misha@mamonov.me>
2023-05-01 11:09:09 +0200

    added some comments

--- examples/mc.sh
+++ examples/mc.sh
@@ -2,7 +2,9 @@ ssh -i .\ec2-private-key.pem ec2-user@ec2-3-69-250-5.eu-central-1.compute.amazon
 
 java -Xmx4G -Xms4G -jar server.jar nogui
 
-hardcoded forge install, ACL on a bucket need to be fixed or turned on manually
+hardcoded forge version install, ACL on a bucket need to be fixed or turned on manually; Inbound Network ACLs need to be recreated on AWS and fixed in TF
+
+https://contabo.com/en/contabo-api/ - check their TF registry and rework to create machines there
 
  C:\terraform\terraform.exe plan
  C:\terraform\terraform.exe apply --auto-approve 
