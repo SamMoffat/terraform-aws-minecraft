9f8b5bdca079000652690f5f3378a88e925ccbfe
Misha Mamonov <misha@mamonov.me>
2023-05-01 10:53:37 +0200

    finalised for now, settled on some manual modding

--- examples/mc.sh
+++ examples/mc.sh
@@ -1,3 +1,8 @@
-ssh -i .\ec2-private-key.pem ec2-user@ec2-18-185-178-186.eu-central-1.compute.amazonaws.com
+ssh -i .\ec2-private-key.pem ec2-user@ec2-3-69-250-5.eu-central-1.compute.amazonaws.com
 
 java -Xmx4G -Xms4G -jar server.jar nogui
+
+hardcoded forge install, ACL on a bucket need to be fixed or turned on manually
+
+ C:\terraform\terraform.exe plan
+ C:\terraform\terraform.exe apply --auto-approve 
